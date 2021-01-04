409e1dc863a5ce0243a4c12fc7793f912240e730
Kwon Eun Jee <passby_time@naver.com>
2021-01-05 02:16:01 +0900

    feat. 비디오 업로드 성공 실패에 대한 처리

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import DropZone from 'react-dropzone';
+import Axios from 'axios';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -41,6 +42,24 @@ function VideoUploadPage() {
     setCategory(e.currentTarget.value)
   }
 
+  const onDrop = (files) => {
+    let formData = new FormData();
+        const config = {
+          header: { 'content-type': 'multipart/form-data' }
+        }
+        console.log(files)
+        formData.append("file", files[0])
+
+    Axios.post('/api/video/uploadfiles', formData, config)
+      .then(response => {
+        if(response.data.success) {
+          console.log(response.data)
+        } else {
+          alert('비디오 업로드를 실패했습니다.')
+        }
+      })
+  }
+
   return (
     <div style={{ maxWidth: '700px', margin: '2em auto',}}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -51,9 +70,9 @@ function VideoUploadPage() {
         <div style={{ display: 'flex', justifyContent: 'space-between'}}>
           {/* Drop zone */}
           <DropZone 
-            onDrop 
-            multiple
-            maxSize
+            onDrop={onDrop}
+            multiple={false}
+            maxSize={10000000}
             >
             {({ getRootProps, getInputProps}) => (
               <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', 
