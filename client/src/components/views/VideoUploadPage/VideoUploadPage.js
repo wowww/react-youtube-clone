dfa3621aacd41399b00f54dc14b3df561276aa6d
Kwon Eun Jee <passby_time@naver.com>
2021-01-05 00:45:35 +0900

    feat. DropZone 다운로드 및 사용

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import DropZone from 'react-dropzone';
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -17,7 +18,19 @@ function VideoUploadPage() {
       <Form onSubmit>
         <div style={{ display: 'flex', justifyContent: 'space-between'}}>
           {/* Drop zone */}
-          
+          <DropZone 
+            onDrop 
+            multiple
+            maxSize
+            >
+            {({ getRootProps, getInputProps}) => (
+              <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', 
+              display: 'flex', alignItems: 'center',justifyContent: 'center'}} {...getRootProps()}>
+                <input {...getInputProps} />
+                <Icon type='plus' style={{ fontSize: '3rem' }} />
+              </div>
+            )}
+            </DropZone>
           {/* Thumbnail */}
           <div>
             <img src alt />
